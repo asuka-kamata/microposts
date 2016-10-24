3072abb8bb95da994047e59bb9fad16399a6159c
asuka kamata <etoileazukiwestgodjp1122.2211@gmail.com>
2016-10-24 21:57:02 +0000

    add home view's timeline

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -9,6 +9,7 @@ class MicropostsController < ApplicationController
             flash[:success] = "Micropost create!"
             redirect_to root_url
         else  
+            @feed_items = current_user.feed_items.include(:user).order(created_at: :desc)
             render 'static_pages/home'
         end
     end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,8 @@
 class StaticPagesController < ApplicationController
   def home
-    # ログインしている場合、新しいMicropostクラスのインスタンスを
-    # user_idに紐付けた状態で初期化
-    @micropost = current_user.microposts.build if logged_in?
+    if logged_in?
+    @micropost = current_user.microposts.build
+    @feed_items = current_user.feed_items.includes(:user).order(created_at: :desc)
+    end
   end
 end
